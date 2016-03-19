9335890b94b91c43c8ad8866ee7d8269201dfaff
Nicolás Vazquez <nicolas.vazquez@fdvsolutions.com>
2016-03-19 03:30:15 -0300

    Setup of the PostgreSQL database on Heroku and configuration of the connection in the server

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
+var config = require('./config');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -17,4 +19,12 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+pg.defaults.ssl = true;
 
+pg.connect(config.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;').on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
---
+++ config.js
@@ -0,0 +1,4 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'postgres://yrxgbqpkfkwsvu:qP8LkCKjPJg6rgotkb5UdL_b8n@ec2-54-83-22-48.compute-1.amazonaws.com:5432/dant70ladmc9ic';
